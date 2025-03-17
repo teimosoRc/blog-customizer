636542d6578ce6743494af055bb2ca15a6d32ba3
teimosoRC <suinagato@gmail.com>
2025-03-17 14:42:57 +0300

    add metod for form, add logic change style

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,20 +16,55 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const [currentStyle, setCurrentStyle] = useState<ArticleStateType>({
+		fontFamilyOption: defaultArticleState.fontFamilyOption,
+		fontColor: defaultArticleState.fontColor,
+		backgroundColor: defaultArticleState.backgroundColor,
+		contentWidth: defaultArticleState.contentWidth,
+		fontSizeOption: defaultArticleState.fontSizeOption,
+	});
+
+	const [selectedFormOptions, setSelectedFormOptions] =
+		useState<ArticleStateType>(currentStyle);
+
+	const toggleForm = () => {
+		setIsOpen((prev) => !prev);
+	};
+
+	const closeForm = () => {
+		setIsOpen(false);
+	};
+	const resetForm = () => {
+		setSelectedFormOptions(defaultArticleState);
+		setCurrentStyle(defaultArticleState);
+	};
+
+	const confirmForm = (e: React.FormEvent) => {
+		e.preventDefault();
+		setCurrentStyle(selectedFormOptions);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': currentStyle.fontFamilyOption.value,
+					'--font-size': currentStyle.fontSizeOption.value,
+					'--font-color': currentStyle.fontColor.value,
+					'--container-width': currentStyle.contentWidth.value,
+					'--bg-color': currentStyle.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
-			<Article />
+			<ArticleParamsForm
+				isOpen={isOpen}
+				toggleForm={toggleForm}
+				formOptions={{ selectedFormOptions, setSelectedFormOptions }}
+				formSubmit={confirmForm}
+				formReset={resetForm}
+			/>
+			<Article closeForm={closeForm} />
 		</main>
 	);
 };
