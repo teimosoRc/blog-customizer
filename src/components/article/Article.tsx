ab364140e8492a5409bc59d7b52de6c2d866f88c
teimosoRC <suinagato@gmail.com>
2025-03-17 14:40:35 +0300

    add logic close sidebar

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -6,9 +6,12 @@ import { Text } from 'src/ui/text';
 
 import styles from './Article.module.scss';
 
-export const Article = () => {
+export const Article = ({ closeForm }: { closeForm: () => void }) => {
+	const clickHandler = () => {
+		closeForm();
+	};
 	return (
-		<article className={clsx(styles.article)}>
+		<article className={clsx(styles.article)} onClick={clickHandler}>
 			<Text as='h1' size={45} weight={800} uppercase dynamicLite>
 				Портрет Западной Швейцарии
 			</Text>
